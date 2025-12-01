b2995a5098783a0cf60b3e2d513de68c771d4cbb
thalita <dev.thalita@gmail.com>
2025-11-30 21:08:33 -0300

    init: commit inicial

---
+++ src/main/java/com/agendou/agendou_api/AgendouApiApplication.java
@@ -0,0 +1,13 @@
+package com.agendou.agendou_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendouApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendouApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendou/agendou_api/AgendouApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendou.agendou_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendouApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
